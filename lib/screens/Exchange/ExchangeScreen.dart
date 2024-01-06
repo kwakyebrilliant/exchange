eb1a52d910927cb827b7ed21005a2205125fb6f9
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-01-06 20:16:33 +0000

    updates

--- lib/screens/Exchange/ExchangeScreen.dart
+++ lib/screens/Exchange/ExchangeScreen.dart
@@ -35,16 +35,66 @@ class _ExchangeScreenState extends State<ExchangeScreen> {
 
                 //Big Container
                 Padding(
-                  padding: const EdgeInsets.only(
-                    top: 20.0,
-                  ),
+                  padding: const EdgeInsets.only(top: 20.0),
                   child: Container(
                     width: MediaQuery.of(context).size.width,
+                    padding: const EdgeInsets.all(16),
                     decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
                       color: const Color(0xFF262B34),
-                      borderRadius: BorderRadius.circular(20.0),
                     ),
-                    child: Text('data'),
+                    child: const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Cardholder Name',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 10,
+                                  ),
+                                ),
+                                SizedBox(height: 4),
+                                Text(
+                                  'John Doe', // Replace with actual cardholder name
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Expires',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 10,
+                                  ),
+                                ),
+                                SizedBox(height: 4),
+                                Text(
+                                  '12/24', // Replace with actual cardholder name
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
