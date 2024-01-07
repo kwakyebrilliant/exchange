f0e23ab975a0d5c6a43c5bc786c24149ce1f672c
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-01-07 17:09:11 +0000

    updates

--- lib/screens/Profile/ProfileScreen.dart
+++ lib/screens/Profile/ProfileScreen.dart
@@ -10,8 +10,163 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFF0D0F14),
+    return Scaffold(
+      backgroundColor: const Color(0xFF0D0F14),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF0D0F14),
+        elevation: 0,
+        title: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                height: 35.0,
+                width: 35.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  image: const DecorationImage(
+                    image: AssetImage(
+                      'assets/images/boy.jpeg',
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Text(
+                  'John Doe',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 15.0,
+            right: 15.0,
+            top: 30.0,
+          ),
+          child: Container(
+            child: Column(
+              children: [
+                //Top cards here
+                Padding(
+                  padding: const EdgeInsets.only(top: 5.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        height: 110.0,
+                        width: 110.0,
+                        decoration: BoxDecoration(
+                          color: Colors.blueGrey.withOpacity(0.2),
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                        child: const Center(
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(top: 25.0),
+                                child: Icon(
+                                  Icons.web_asset,
+                                  color: Colors.white,
+                                  size: 30,
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(top: 10.0),
+                                child: Text(
+                                  'Assets',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+
+                      //favourite here
+                      Container(
+                        height: 110.0,
+                        width: 110.0,
+                        decoration: BoxDecoration(
+                          color: Colors.blueGrey.withOpacity(0.2),
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                        child: const Center(
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(top: 25.0),
+                                child: Icon(
+                                  Icons.add,
+                                  color: Colors.white,
+                                  size: 30,
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(top: 10.0),
+                                child: Text(
+                                  'Deposit',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+
+                      //settings
+                      Container(
+                        height: 110.0,
+                        width: 110.0,
+                        decoration: BoxDecoration(
+                          color: Colors.blueGrey.withOpacity(0.2),
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                        child: const Center(
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(top: 25.0),
+                                child: Icon(
+                                  Icons.settings,
+                                  color: Colors.white,
+                                  size: 30,
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(top: 10.0),
+                                child: Text(
+                                  'Settings',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
